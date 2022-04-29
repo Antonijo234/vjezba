0f044ed9c97fd36967a44b493da8ae5f62132018
Antonijo <antonijo.brezak@gmail.com>
2022-04-29 09:18:42 +0200

    set up public mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,9 @@
+class ContactMailer < ActionMailer::Base
+    default to: "info@chesnowitz.com"
+    def contact_email(name, email, message)
+      @name = name
+      @email = email
+      @message = message
+      mail(from: email, subject: 'Articles Contact Form Message')
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, controller: {registrations: 'registrations'}
   resources :articles
+  resources :contacts
   root to:'pages#index'
   get 'pages/contact'
   get 'pages/about'
