202bd17676be1e8dbbd6c027f43fb92501225b68
Antonijo <antonijo.brezak@gmail.com>
2022-05-01 12:24:53 +0200

    almost done2

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,22 @@
-class CommentController < ApplicationController
+class CommentsController < ApplicationController
 before_action :authenticate_user!
 
   def create
-    @article =Article.find(Params[:article_id])
-    @comment =@article.comment.create(Params[:comment].permit(:body))
-    @comment.user_id=current_user.id if current_user
-
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(params[:comment].permit(:body))
+    @comment.user_id = current_user.id if current_user
     if @comment.save
-      redirect_to article_path(@article), notice:"Your comment has been saved."
+      redirect_to article_path(@article), notice: "Your comment has been saved."
     else
       redirect_to 'new'
-    end
+end
   end
 
   def update
-    @article =Article.find(Params[:article_id])
-    @comment=@article.comment.find(Params[:id])
+    @article =Article.find(params[:article_id])
+    @comment=@article.comment.find(params[:id])
 
-    if @comment.update(Params[:comment].permit(:body))
+    if @comment.update(params[:comment].permit(:body))
       redirect_to article_path(@article), notice:"Your comment has been updated."
     else
       render 'edit'
@@ -25,13 +24,13 @@ before_action :authenticate_user!
   end
 
   def edit
-    @article =Article.find(Params[:article_id])
-    @comment=@article.comment.find(Params[:id])
+    @article =Article.find(params[:article_id])
+    @comment=@article.comment.find(params[:id])
   end
 
   def destroy
-    @article =Article.find(Params[:article_id])
-    @comment=@article.comment.find(Params[:id])
+    @article =Article.find(params[:article_id])
+    @comment=@article.comment.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article), notice:"Your comment has been deleted."
 
