5cd25372b4034f2909ff4aea883d347639ad5f54
Antonijo <antonijo.brezak@gmail.com>
2022-05-23 14:09:02 +0200

    using let instead of variables

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-     it "must have a email" do
-       user=FactoryBot.create(:user)
-       expect(user.email).to eq("mystring@gmail.com")
-     end
-     it "must have an password confirmation same as password" do
-       user=FactoryBot.create(:user)
-       expect(user.password).to eq(user.password_confirmation)
-     end
+  let(:user) { FactoryBot.create(:user)  }
 
-   end
+  it "must have a email" do
+    expect(user.email).to eq("mystring@gmail.com")
+  end
+
+  it "must have an password confirmation same as password" do
+    expect(user.password).to eq(user.password_confirmation)
+end
+end
